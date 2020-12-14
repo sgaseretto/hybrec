0cffd8f516b43659d1c50fde7e08515f09d397fd
Sebastian Gonzalez Aseretto <sgaseretto@gmail.com>
2020-12-14 16:09:30 -0300

    Added SynthDataset to build mockdatasets

--- hybrec/_nbdev.py
+++ hybrec/_nbdev.py
@@ -2,9 +2,17 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"get_uniques": "03_prep.ipynb"}
+index = {"SynthDataset": "01_synth_dataset.ipynb",
+         "gen_values": "01_synth_dataset.ipynb",
+         "gen_added_n_deleted": "01_synth_dataset.ipynb",
+         "exclude_element": "01_synth_dataset.ipynb",
+         "build_interactions": "01_synth_dataset.ipynb",
+         "build_features_from_df": "01_synth_dataset.ipynb",
+         "SynthDataset.build_synth_dataset": "01_synth_dataset.ipynb",
+         "get_uniques": "04_prep.ipynb"}
 
-modules = ["prep.py"]
+modules = ["synth.py",
+           "prep.py"]
 
 doc_url = "https://sgaseretto.github.io/hybrec/"
 
