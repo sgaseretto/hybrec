c18847d2d4c63a2846c74e3433be9d8ef93b5177
Sebastian Gonzalez Aseretto <sgaseretto@gmail.com>
2020-12-22 10:47:34 -0300

    Renamed SynthDataset to SynthDatasetGenerator  since the class is used to generate to Datasets, and is not a Dataset itself

--- hybrec/_nbdev.py
+++ hybrec/_nbdev.py
@@ -2,23 +2,23 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"SynthDataset": "01_synth_dataset.ipynb",
+index = {"SynthDatasetGenerator": "01_synth_dataset.ipynb",
          "gen_values": "01_synth_dataset.ipynb",
          "gen_added_n_deleted": "01_synth_dataset.ipynb",
          "exclude_element": "01_synth_dataset.ipynb",
          "build_interactions": "01_synth_dataset.ipynb",
          "build_metadata_from_df": "01_synth_dataset.ipynb",
-         "SynthDataset.add_user_metadata": "01_synth_dataset.ipynb",
-         "SynthDataset.add_item_metadata": "01_synth_dataset.ipynb",
-         "SynthDataset.gen_users_n_items": "01_synth_dataset.ipynb",
-         "SynthDataset.gen_metadata": "01_synth_dataset.ipynb",
-         "SynthDataset.print_dataset_components": "01_synth_dataset.ipynb",
-         "SynthDataset.gen_before_n_after_datasets": "01_synth_dataset.ipynb",
-         "SynthDataset.build_synth_dataset": "01_synth_dataset.ipynb",
-         "SynthDataset.build_user_item_interactions": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.add_user_metadata": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.add_item_metadata": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.gen_users_n_items": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.gen_metadata": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.print_dataset_components": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.gen_before_n_after_datasets": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.build_synth_dataset": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.build_user_item_interactions": "01_synth_dataset.ipynb",
          "df_highlight_interaction": "01_synth_dataset.ipynb",
          "df_color_positive_interaction": "01_synth_dataset.ipynb",
-         "SynthDataset.top_interactions_crosstab": "01_synth_dataset.ipynb",
+         "SynthDatasetGenerator.top_interactions_crosstab": "01_synth_dataset.ipynb",
          "get_uniques": "04_prep.ipynb"}
 
 modules = ["synth.py",
